be6149be6a8c80bb6e3c15cbc03578f55c39b277
Arzival <arzival.sistemas@gmail.com>
2021-07-12 17:58:24 -0500

    fin del menu desplegable

---
+++ scripts.js
@@ -0,0 +1,13 @@
+let listElement = document.querySelectorAll('.list__button--click');
+
+listElement.forEach(listElement => {
+          listElement.addEventListener('click', ()=> {
+                    listElement.classList.toggle('arrow');
+                    let height = 0;
+                    let menu = listElement.nextElementSibling;
+                    if (menu.clientHeight == 0) {
+                              height = menu.scrollHeight ;
+                    }
+                    menu.style.height = height+'px';
+          })
+});
